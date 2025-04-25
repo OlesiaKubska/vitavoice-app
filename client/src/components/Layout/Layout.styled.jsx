300df5a302dd833b11c5887444e3fd007de1d7ce
OlesiaKubska <kublesia0908@gmail.com>
2025-04-26 00:02:19 +0200

    Fixed styles

--- client/src/components/Layout/Layout.styled.jsx
+++ client/src/components/Layout/Layout.styled.jsx
@@ -7,7 +7,6 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-  border: 1px solid ${({ theme }) => theme.border};
 `;
 
 export const Main = styled.main`
